541534f4dfa4bb8cfbb7a8f70744278ee285145b
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-10 08:58:44 -0300

    Add "scramble" function for testing

--- test/flex_server_test.clj
+++ test/flex_server_test.clj
@@ -2,7 +2,19 @@
   (:require [clojure.test :refer :all]
             [flex-server :refer [scramble?]]))
 
+(defn scramble [word]
+  (-> (seq word) shuffle clojure.string/join))
+
 (deftest test-challenge-examples
   (is (true? (scramble? "rekqodlw" "world")))
   (is (true? (scramble? "cedewaraaossoqqyt" "codewars")))
   (is (false? (scramble? "katas" "steak"))))
+
+(deftest test-simplistic-examples
+  (is (false? (scramble? "recogized" "recognized")))
+  (is (true? (scramble? "recognnized" "recognized"))))
+
+(deftest test-shuffles
+  (is (true?
+       (let [word "unpredictable"]
+         (scramble? (scramble word) word)))))
