e924debbca26b283cc4cac8c3aa878c1f2b82c55
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-11 20:13:16 -0300

    Generalize function to mock requests

--- test/flex_server_test.clj
+++ test/flex_server_test.clj
@@ -9,33 +9,52 @@
   (-> (seq word) shuffle clojure.string/join))
 
 (defn mk-req
-  "str, str -> Response map
-  Makes a mocked request to flex-server's handler and returns its response.
+  "Optional[map] -> Response map
+  Makes a request to flex-server's handler and returns its response.
   If the body contains JSON, the function parses the content before returning
   the response."
+  ([]
+   (mk-req nil))
+  ([params]
+   (let [resp (web-app (mock/request :get "/is-scrambled" params))]
+     (if (re-find #"application/json" (get-in resp [:headers "Content-Type"]))
+       (update resp :body #(with-open [s (clojure.java.io/reader %)]
+                             (parse-stream s true)))
+       resp))))
+
+(defn mk-valid-req
+  "str, str -> Response map"
   [scrambled word]
-  (let [resp (->> {"scrambled" scrambled "word" word}
-                  (mock/request :get "/is-scrambled")
-                  web-app)]
-    (if (re-find #"application/json" (get-in resp [:headers "Content-Type"]))
-      (update resp :body #(with-open [s (clojure.java.io/reader %)]
-                            (parse-stream s true)))
-      resp)))
+  (mk-req {"scrambled" scrambled "word" word}))
 
 
-(deftest test-simplistic-examples
-  (is (false? (scramble? "recogized" "recognized")))
-  (is (true? (scramble? "recognnized" "recognized"))))
+(testing "Only the primary function of the system"
+ (deftest test-simplistic-examples
+   (is (false? (scramble? "recogized" "recognized")))
+   (is (true? (scramble? "recognnized" "recognized"))))
 
-(deftest test-shuffles
-  (let [samples (remove empty? (gen/sample gen/string-alphanumeric))]
-    (is (every? #(scramble? (scramble %) %) samples))))
+ (deftest test-shuffles
+   (let [samples (remove empty? (gen/sample gen/string-alphanumeric))]
+     (is (every? #(scramble? (scramble %) %) samples)))))
 
-(deftest test-challenge-examples-with-handler
-  (letfn [(check [scrambled word bool-fn]
-            (let [resp (mk-req scrambled word)]
-              (is (== 200 (:status resp)))
-              (is (bool-fn (get-in resp [:body :scrambled?])))))]
-    (check "rekqodlw" "world" true?)
-    (check "cedewaraaossoqqyt" "codewars" true?)
-    (check "katas" "steak" false?)))
+(testing "The system using its web API"
+  (deftest test-challenge-examples-with-handler
+    (letfn [(check [scrambled word bool-fn]
+              (let [resp (mk-valid-req scrambled word)]
+                (is (== 200 (:status resp)))
+                (is (bool-fn (get-in resp [:body :scrambled?])))))]
+      (check "rekqodlw" "world" true?)
+      (check "cedewaraaossoqqyt" "codewars" true?)
+      (check "katas" "steak" false?)))
+
+  (deftest test-nonexistent-route
+    (let [resp (web-app (mock/request :get "/anything"))]
+      (is (== 404 (:status resp)))))
+
+  (deftest test-nonexistent-verb
+    (let [resp (web-app (mock/request :post "/is-scrambled"))]
+      (is (== 405 (:status resp)))))
+
+  (deftest test-invalid-requests
+    ;; Missing parameters.
+    (is (== 400 (:status (mk-req))))))
