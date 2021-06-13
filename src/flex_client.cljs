ea9e134e743fa51360a655fdc3136e1019d8bc80
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-13 18:31:33 -0300

    Add simple HTML with a form

--- src/flex_client.cljs
+++ src/flex_client.cljs
@@ -1,3 +1,12 @@
-(ns flex-client)
+(ns flex-client
+  (:require [ajax.core :refer [GET]]))
 
-(println "hello world")
+(defn get-elem-val [id]
+  (.-value (.getElementById js/document id)))
+
+(defn is-scrambled [scrambled-id word-id]
+  (let [scrambled-val (get-elem-val scrambled-id)
+        word-val (get-elem-val word-id)]
+    (println scrambled-val word-val)
+    (js/alert scrambled-val word-val)
+    ))
