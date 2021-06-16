44da22fd6f8608885263964af6d30c69bf6a9620
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-16 12:01:59 -0300

    Replace an HTML text with the back-end answer

--- src/flex_client.cljs
+++ src/flex_client.cljs
@@ -4,12 +4,17 @@
 (defn get-elem-val [id]
   (.-value (.getElementById js/document id)))
 
-(defn is-scrambled [scrambled-id word-id]
+(defn is-scrambled
+  "str, str, str -> nil
+  Gets the ID of two input elements and one text element, checks if the value
+  is scramble using the back-end, and replaces the text with the answer."
+  [scrambled-id word-id answer-id]
   (let [scrambled-val (get-elem-val scrambled-id)
         word-val (get-elem-val word-id)]
-    (.log js/console scrambled-val word-val)
+    ;; TODO: parameterize the back-end address.
     (GET "http://localhost:3000/is-scrambled"
          {:params {:scrambled scrambled-val
                    :word word-val}
-          :handler #(.log js/console (str %))})
-    ))
+          :handler #(-> (.getElementById js/document answer-id)
+                        .-textContent
+                        (set! (:scrambled? %)))})))
