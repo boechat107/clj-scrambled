d9018431079f736ad03a5d332ed43e2ca46694a7
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-17 08:40:59 -0300

    Add reagent components to render the page

--- src/flex_client.cljs
+++ src/flex_client.cljs
@@ -1,5 +1,7 @@
 (ns flex-client
-  (:require [ajax.core :refer [GET]]))
+  (:require [ajax.core :refer [GET]]
+            [reagent.core :as r]
+            [reagent.dom :as rd]))
 
 (defn get-elem-val [id]
   (.-value (.getElementById js/document id)))
@@ -17,3 +19,24 @@
           :handler #(-> (.getElementById js/document answer-id)
                         .-textContent
                         (set! (:scrambled? %)))})))
+
+(defn mk-text-input [id-name]
+  [:input.form-control.mb-2.mr-sm-2
+   {:type "text" :id id-name :name id-name :placeholder id-name}])
+
+(defn form-component []
+  [:form.form-inline
+   (mk-text-input "scrambled")
+   (mk-text-input "word")])
+
+(defn page []
+  [:div.container.pt-3
+   [:div.jumbotron>h1 "Scrambled?"]
+   [form-component]
+   [:p "Is it scrambled?"]])
+
+(defn init []
+  (rd/render [page]
+             (.getElementById js/document "primary-container")))
+
+(init)
