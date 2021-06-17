b041e89c8e8c8894b4da527e9b35c317407441b9
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-06-17 20:56:11 -0300

    Use Schema to define records; use different colors for different answers

--- src/flex_client.cljs
+++ src/flex_client.cljs
@@ -1,11 +1,17 @@
 (ns flex-client
   (:require [ajax.core :refer [GET]]
             [reagent.core :as r]
-            [reagent.dom :as rd]))
+            [reagent.dom :as rd]
+            [schema.core :as s :include-macros true]))
 
-(defrecord FormData [scrambled word answer])
+(s/defrecord FormData
+    [scrambled :- s/Str
+     word :- s/Str
+     answer :- s/Str])
 
-(defrecord RequestData [scrambled word])
+(s/defrecord RequestData
+    [scrambled :- s/Str
+     word :- s/Str])
 
 (letfn [(on-change [elem input-key atom-data]
           (let [elem-val (-> elem .-target .-value)
@@ -26,13 +32,15 @@
 (defn form-component []
   (let [atom-data (r/atom (FormData. "" "" ""))]
     (fn []
-      [:div
-       [:div.jumbotron>h1 "Scrambled?"]
-       [:form.form-inline
-        (mk-text-input "scrambled" atom-data)
-        (mk-text-input "word" atom-data)]
-       [:p "Is it scrambled? "
-        [:span.text-success (:answer @atom-data)]]])))
+      (let [answer (:answer @atom-data)]
+        [:div
+         [:div.jumbotron>h1 "Scrambled?"]
+         [:form.form-inline
+          (mk-text-input "scrambled" atom-data)
+          (mk-text-input "word" atom-data)]
+         [:p "Is it scrambled? "
+          [:span {:class (if (== answer "true") "text-success" "text-danger")}
+           answer]]]))))
 
 (defn init []
   (rd/render [form-component]
